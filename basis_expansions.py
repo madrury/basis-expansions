2a712d1ce90155e6716842e46185d5de4ff810b1
madrury <matthew.drury.83@gmail.com>
2017-07-07 16:51:22 -0700

    Add docstring for Binner.

--- basis_expansions.py
+++ basis_expansions.py
@@ -3,8 +3,24 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class Binner(BaseEstimator, TransformerMixin):
+    """Apply a binning basis expansion to an array.
 
-    def __init__(self, min, max, cutpoints=None, n_bins=None):
+    Create new features out of an array by binning the values of that array
+    based on a sequence of intervals. An indicator feature is created for each
+    bin, indicating which bin the given observation falls into.
+    
+    This transformer can be created by sepcifying the maximum, minimum, and 
+    number of cutpoints, or by specifying the cutpoints directly.
+
+    Parameters
+    ----------
+    min: Minimum cutpoint for the bins.
+    max: Maximum cutpoint for the bins.
+    n_bins: The number of bins to create.
+    cutpoints: The cutpoints to ceate.
+    """
+
+    def __init__(self, min=None, max=None, n_bins=None, cutpoints=None):
         self._min = min
         self._max = max
         if not cutpoints:
