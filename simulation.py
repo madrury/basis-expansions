89ae2a608f84a8a554f08485cda955912ac693c1
madrury <matthew.drury.83@gmail.com>
2017-07-23 17:13:08 -0700

    Complete all simulation experiments.

--- simulation.py
+++ simulation.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def run_simulation_expreiment(signal, regressors, N=250, N_trials=20):
-    trials = run_trials(signal, regressors, N, N_trials)
+def run_simulation_expreiment(signal, regressors, N=250, N_trials=20, sd=0.1):
+    trials = run_trials(signal, regressors, N, N_trials, sd)
     
     def make_mean_errors(trials):
         train_mean_errors = trials[0].mean(axis=0)
@@ -32,20 +32,20 @@ def plot_simulation_expreiment(ax, degrees_of_freedom, mean_errors, std_errors):
         degrees_of_freedom, test_top_band, test_bottom_band, alpha=0.2)
     ax.legend()
 
-def make_random_train_test(signal, N=250):
+def make_random_train_test(signal, N=250, sd=0.1):
     x_train = np.random.uniform(0, 1, size=N)
-    y_train = signal(x_train) + np.random.normal(scale=0.1, size=N)
+    y_train = signal(x_train) + np.random.normal(scale=sd, size=N)
     x_test = np.random.uniform(0, 1, size=N)
-    y_test = signal(x_test) + np.random.normal(scale=0.1, size=N)
+    y_test = signal(x_test) + np.random.normal(scale=sd, size=N)
     return (x_train, y_train), (x_test, y_test)
 
-def run_trials(signal, regressors, N=250, N_trials=250):
+def run_trials(signal, regressors, N=250, N_trials=250, sd=0.1):
     N_dof = len(regressors)
     train_errors = np.empty(shape=(N_trials, N_dof))
     test_errors = np.empty(shape=(N_trials, N_dof))
     for i in range(N_trials):
         for j, regressor in enumerate(regressors):
-            (x, y), (x_test, y_test) = make_random_train_test(signal, N)
+            (x, y), (x_test, y_test) = make_random_train_test(signal, N, sd)
             regressor.fit(x.reshape(-1, 1), y)
             y_hat_train, y_hat_test = (regressor.predict(x.reshape(-1, 1)), 
                                        regressor.predict(x_test.reshape(-1, 1)))
