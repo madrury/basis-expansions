779ed3fc3f10398c46cc4158807b70657f4146b2
madrury <matthew.drury.83@gmail.com>
2017-08-13 20:58:41 -0700

    Fix indexing bug in NaturalCubicSpline

--- basis_expansions.py
+++ basis_expansions.py
@@ -225,15 +225,25 @@ class NaturalCubicSpline(AbstractSpline):
 
     @property
     def n_params(self):
-        return self.n_knots
+        return self.n_knots - 1
 
     def transform(self, X, **transform_params):
-        ppart = lambda t: np.maximum(0, t)
-        cube = lambda t: t*t*t
-        X_spl = np.zeros((X.shape[0], self.n_knots))
+        X_spl = self._transform_array(X)
+        if isinstance(X, pd.Series):
+            col_names = ["{}_spline_linear".format(X.name)] + [
+                "{}_spline_{}".format(X.name, idx)
+                for idx in range(self.n_knots - 2)]
+            X_spl = pd.DataFrame(X_spl, columns=col_names, index=X.index)
+        return X_spl
+
+    def _transform_array(self, X, **transform_params):
+        X = X.squeeze()
+        X_spl = np.zeros((X.shape[0], self.n_knots - 1))
         X_spl[:, 0] = X.squeeze()
 
         def d(knot_idx, x):
+            ppart = lambda t: np.maximum(0, t)
+            cube = lambda t: t*t*t
             numerator = (cube(ppart(x - self.knots[knot_idx]))
                             - cube(ppart(x - self.knots[self.n_knots - 1])))
             denominator = self.knots[self.n_knots - 1] - self.knots[knot_idx]
@@ -241,5 +251,4 @@ class NaturalCubicSpline(AbstractSpline):
 
         for i in range(0, self.n_knots - 2):
             X_spl[:, i+1] = (d(i, X) - d(self.n_knots - 2, X)).squeeze()
-
         return X_spl
